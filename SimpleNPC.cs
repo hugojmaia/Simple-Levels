0197b0a915b57ede803064360e34f1afd28914ca
hugojmaia <hugojmaia@hotmail.com>
2021-11-12 13:03:52 -0300

    2.0.0 update
    Comment changes

--- SimpleNPC.cs
+++ SimpleNPC.cs
@@ -6,24 +6,26 @@ using Terraria.ModLoader;
 /*
  * Calculating and awarding xp each time something dies.
  * XP is only awarded if at least one player participated in killing the npc.
+ * XP won't be awarded if the enemy is invulnerable to damage in any part of it. This is the simplest method I could find to address the Crawltipede's inflated xp, along with any attempts to reuse that enemy by other mods.
+ * The XPCap variable is there in case anyone wants to set limits on things.
  */
 
 namespace SimpleLevels
 {
     public class SimpleNPC : GlobalNPC
-	{
+    {
         public double XPCap = 0.0;
         
         public override bool InstancePerEntity
-		{
-			get
-			{
-				return true;
-			}
-		}
+        {
+            get
+            {
+                return true;
+            }
+        }
 
-		public override void NPCLoot(NPC npc)
-		{
+        public override void NPCLoot(NPC npc)
+        {
             double XP;
 
             XP = Math.Pow(npc.lifeMax * npc.damage * Math.Max((double)npc.defense, 1.0), 0.5);
@@ -43,11 +45,11 @@ namespace SimpleLevels
                     packet.Write(XP);
                     packet.Send();
                 }
-                else if (Main.netMode == 0)
+                if (Main.netMode == 0)
                 {
                     Main.LocalPlayer.GetModPlayer<SimplePlayer>().AddXP(XP);
                 }
             }
         }
-	}
+    }
 }
