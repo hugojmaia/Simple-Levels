8afe8144f53d8882788bdadb9377c876edbcb120
hugojmaia <hugojmaia@hotmail.com>
2023-08-12 18:23:48 -0300

    Update to 2.1.0
    Removed the xp cap here, don't think it was interesting.
    The exponent for the xp can now be configured.

--- SimpleNPC.cs
+++ SimpleNPC.cs
@@ -7,15 +7,12 @@ using Terraria.ModLoader;
  * Calculating and awarding xp each time something dies.
  * XP is only awarded if at least one player participated in killing the npc.
  * XP won't be awarded if the enemy is invulnerable to damage in any part of it. This is the simplest method I could find to address the Crawltipede's inflated xp, along with any attempts to reuse that enemy by other mods.
- * The XPCap variable is there in case anyone wants to set limits on things.
  */
 
 namespace SimpleLevels
 {
     public class SimpleNPC : GlobalNPC
     {
-        public double XPCap = 0.0;
-        
         public override bool InstancePerEntity
         {
             get
@@ -24,23 +21,21 @@ namespace SimpleLevels
             }
         }
 
-        public override void NPCLoot(NPC npc)
+        public override void OnKill(NPC npc)
         {
             double XP;
 
-            XP = Math.Pow(npc.lifeMax * npc.damage * Math.Max((double)npc.defense, 1.0), 0.5);
+            XP = Math.Pow(npc.lifeMax * npc.damage * Math.Max((double)npc.defense, 1.0), (double)ModContent.GetInstance<SimpleConfig>().MobXPExponent);
             
             if (npc.lifeMax == 1)
                 XP = 0.0;
-            
-            if (XPCap != 0.0)
-                XP = Math.Min(XP, XPCap);
 
             if (npc.lastInteraction != 255 && !npc.dontTakeDamage)
             {
+                //Main.LocalPlayer.GetModPlayer<SimplePlayer>().AddXP(XP);
                 if (Main.netMode == 2)
                 {
-                    ModPacket packet = mod.GetPacket();
+                    ModPacket packet = Mod.GetPacket();
                     packet.Write((byte)SimpleLevels.Message.AddXP);
                     packet.Write(XP);
                     packet.Send();
